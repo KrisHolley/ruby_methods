fb229a490030db867e6ce88c8fac34ef46ce7a05
Kris <krisholley_09@hotmail.com>
2018-10-23 15:58:23 -0500

    trying ideas, putting forth the effort.

--- methods.rb
+++ methods.rb
@@ -1,8 +1,44 @@
-def all_caps
-    hash_caps = {
-    "h" => "H",
-    "e" => "E",
-    "l" => "L",
-    "o" => "O",
-    }
-end
+# def all_caps
+#     hash_caps = {
+#     "a" => "A",
+#     "b" => "B",
+#     "c" => "C",
+#     "d" => "D",
+#     "e" => "E",
+#     "f" => "F",
+#     "g" => "G",
+#     "h" => "H",
+#     "i" => "I",
+#     "j" => "J",
+#     "k" => "K",
+#     "l" => "L",
+#     "m" => "M",
+#     "n" => "N",
+#     "o" => "O",
+#     "p" => "P",
+#     "q" => "Q",
+#     "r" => "R",
+#     "s" => "S",
+#     "t" => "T",
+#     "u" => "U",
+#     "v" => "V",
+#     "w" => "W",
+#     "x" => "X",
+#     "y" => "Y",
+#     "z" => "Z",
+#     }
+# end
+
+
+
+# p all_caps
+
+
+# ("A".."Z").map do |letter|
+#     puts letter
+#   end
+lower = ("a..z")
+    p lower
+
+upper = ("A...Z")
+    p upper
